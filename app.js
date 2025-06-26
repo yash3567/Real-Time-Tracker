997f6dca0ad3cf1e70f723c29dcd65fce3fb8eb4
yash3567 <yashnandeshwar32@gmail.com>
2025-06-26 14:07:35 +0530

    index.ejs update in the root directory

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
+app.set("views", __dirname);
 app.use(express.static(path.join(__dirname, "public")));
 
 
